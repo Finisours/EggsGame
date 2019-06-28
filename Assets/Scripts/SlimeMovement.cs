39b074c5d219728f781d024c3d6f68970306a3ad
Finisours <shkolnik337.yanekok@gmail.com>
2019-06-28 16:19:33 +0300

    first commit

---
+++ Assets/Scripts/SlimeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMovement : MonoBehaviour
+{
+    public GameObject gmj;
+    private float moveSpeed=20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            gmj.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            gmj.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/Tankmovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tankmovement : MonoBehaviour
+{
+
+    public GameObject obj;
+    public float range = 5f, moveSpeed = 3f, turnSpeed = 40f;
+    private Light rise;
+
+    void Start()
+    {
+        rise = GameObject.Find("Light").GetComponent<Light>();
+    }
+
+    void Update(){
+        BodyMovement();
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            print("Light");
+            rise.enabled = !rise.enabled;
+        }
+   
+    }
+    void BodyMovement() {
+       // if (Input.GetKey(KeyCode.UpArrow))
+         //   obj.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        //if (Input.GetKey(KeyCode.DownArrow))
+          //  obj.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            obj.transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            obj.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+
+}
+
